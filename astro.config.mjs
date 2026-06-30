c5107ebe2f8f8b631af46bee3da95ef20fe0b2d4
Max Carter 祁明思 <undivisible@vk.com>
2026-06-30 10:58:29 +1000

    chore: drop unocss, use layout utility css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import UnoCSS from '@unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [UnoCSS()],
 });
